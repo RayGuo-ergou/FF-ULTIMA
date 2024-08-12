f486ba6707cb772ba3bf93cdb35da9aae5aa5d48
Ray Guo <ma2808203259@hotmail.com>
2024-08-12 14:10:40 +1000

    feat: tweak config

--- user.js
+++ user.js
@@ -8,11 +8,11 @@ user_pref("user.theme.light.gruvbox", false);
 user_pref("user.theme.dark.midnight", false);
 
 /*tabs*/
-user_pref("ultima.tabs.vertical", false);
+user_pref("ultima.tabs.vertical", true);
 user_pref("ultima.tabs.size.xs", false);
 user_pref("ultima.tabs.size.s", false);
 user_pref("ultima.tabs.size.l", true);
-user_pref("ultima.tabs.autohide", false);
+user_pref("ultima.tabs.autohide", true);
 user_pref("ultima.tabs.closetabsbutton", true);
 user_pref("ultima.tabs.vertical.hide", false);
 user_pref("ultima.tabs.vertical.onlyprivate", false);
